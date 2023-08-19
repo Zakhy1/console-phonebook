2f979789ce6502efc3f73e4b6b732f5aedd0ccc9
zakhy <lirchzakhar@gmail.com>
2023-08-19 22:23:50 +0700

    add data.json

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import json
 
+new_peaple = {"name": "Ivan", "surname": "Petrov",
+              "patronymic": "Gagov",
+              "organization-name": "MacDonalds",
+              "work-phone": "87-23-45",
+              "personal-phone": "+7-978-657-76-54"}
+
+to_json = json.dumps(new_peaple)
+
+with open("data.json", "a") as file:
+    data = file.write(to_json)
